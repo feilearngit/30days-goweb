1704a3d37a6aee4f1cdb51ba0cb19e942e79ea8b
feilearngit <36058726+feilearngit@users.noreply.github.com>
2019-06-04 17:19:43 +0800

    cookie
    set and get cookie
    flash message

--- gossl/main.go
+++ gossl/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
 	json2 "encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 type MyHandler struct {
@@ -65,23 +67,85 @@ func process(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintln(w, r.MultipartForm)
 }
 
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co.",
+		HttpOnly: true,
+	}
+
+	//两种cookie设置方式
+	w.Header().Set("Set-Cookie", c1.String())
+	http.SetCookie(w, &c2) //此处传入cookie结构的指针
+
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil{
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
+	//h := r.Header["Cookie"]
+	//fmt.Fprintln(w, h)
+}
+
 func jsonExample(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	post := &Post{
-		User:"feifei jiang",
-		Threads:[]string{"first", "second", "third"},
+		User:    "feifei jiang",
+		Threads: []string{"first", "second", "third"},
 	}
 	json, _ := json2.Marshal(post)
 	w.Write(json)
 }
+
+
 
 func headers(w http.ResponseWriter, r *http.Request) {
 	h := r.Header.Get("Accept-Encoding")
 	fmt.Fprintln(w, h)
 }
 
+
+
 func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
+}
+
+func setMessage(w http.ResponseWriter, r *http.Request){
+	msg := []byte("Hello World!")
+	c := http.Cookie{
+		Name:"flash",
+		Value:base64.URLEncoding.EncodeToString(msg),
+	}
+
+	http.SetCookie(w, &c)
+}
+
+func showMessage(w http.ResponseWriter, r *http.Request){
+	c, err := r.Cookie("flash")
+	if err != nil{
+		if err == http.ErrNoCookie{
+			fmt.Fprintln(w, "No message found")
+		}
+	}else{
+		rc := http.Cookie{
+			Name:"flash",
+			MaxAge:-1,
+			Expires:time.Unix(1,0),
+		}
+		http.SetCookie(w, &rc)
+		val, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(val))
+	}
 }
 
 func main() {
@@ -90,12 +154,17 @@ func main() {
 		Addr: "127.0.0.1:8080",
 		//Handler: &handler,
 	}
+
+	http.HandleFunc("/set_message", setMessage)
+	http.HandleFunc("/show_message", showMessage)
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
 	http.HandleFunc("/body", body)
 	http.HandleFunc("/process", process)
 	http.HandleFunc("/header", headers)
 	http.HandleFunc("/write", writeExample)
-	http.HandleFunc("writeheader", writeHeaderExample)
-	http.HandleFunc("redirect", headerExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
 	http.HandleFunc("/json", jsonExample)
 	//http2.ConfigureServer(&server, &http2.Server{})
 	server.ListenAndServe()
