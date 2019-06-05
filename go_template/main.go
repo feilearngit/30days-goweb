819d2a75b61567016b5adf2166f64873aa6ca3ed
feilearngit <36058726+feilearngit@users.noreply.github.com>
2019-06-05 13:45:59 +0800

    template test

--- go_template/main.go
+++ go_template/main.go
@@ -41,10 +41,22 @@ func pipeX(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, time.Now())
 }
 
+func layoutX(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().Unix())
+	var t *template.Template
+	if rand.Intn(10) > 5 {
+		t, _ = template.ParseFiles("layout.html", "red_hello.html")
+	} else {
+		t, _ = template.ParseFiles("layout.html", "blue_hello.html")
+	}
+	t.ExecuteTemplate(w, "layout", "")
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
+	http.HandleFunc("/layout", layoutX)
 	http.HandleFunc("/pipe", pipeX)
 	http.HandleFunc("/include", includeX)
 	http.HandleFunc("/with", withX)
